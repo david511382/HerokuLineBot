88549623fc05eb778543e0ee3f85504e9be4c072
david511382 <david511382@yahoo.com.tw>
2021-04-13 22:23:09 +0800

    [修改] 資料表 activity 欄位 courts_and_time 大小

--- storage/database/database/clubdb/table/activity/model.go
+++ storage/database/database/clubdb/table/activity/model.go
@@ -6,7 +6,7 @@ type ActivityTable struct {
 	ID            int       `gorm:"column:id;type:serial;primary_key;not null"`
 	Date          time.Time `gorm:"column:date;type:date;not null;index"`
 	Place         string    `gorm:"column:place;type:varchar(50);not null"`
-	CourtsAndTime string    `gorm:"column:courts_and_time;type:varchar(50);not null"`
+	CourtsAndTime string    `gorm:"column:courts_and_time;type:varchar(200);not null"`
 	MemberCount   int16     `gorm:"column:member_count;type:smallint;not null"`
 	GuestCount    int16     `gorm:"column:guest_count;type:smallint;not null"`
 	MemberFee     int16     `gorm:"column:member_fee;type:smallint;not null"`
