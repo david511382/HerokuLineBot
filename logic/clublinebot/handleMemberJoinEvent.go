a393ead405ebcb83b21b1ea23e6f7869a09786c2
david511382 <david511382@yahoo.com.tw>
2021-04-30 20:39:18 +0800

    [修改] line bot 加入群組查詢用戶資料錯誤處理

--- logic/clublinebot/handleMemberJoinEvent.go
+++ logic/clublinebot/handleMemberJoinEvent.go
@@ -1,7 +1,6 @@
 package clublinebot
 
 import (
-	"encoding/json"
 	"fmt"
 	"heroku-line-bot/service/linebot"
 	lineBotModel "heroku-line-bot/service/linebot/domain/model"
@@ -13,41 +12,6 @@ func (b *ClubLineBot) handleMemberJoinedEvent(event *lineBotModel.MemberJoinEven
 	replyToken := event.ReplyToken
 	if err := b.tryLine(
 		func() error {
-			groupID := event.Source.GroupID
-			pushMessages := []interface{}{
-				linebot.GetTextMessage("member join group : " + groupID),
-			}
-
-			userInfoMsgs := []string{}
-			for _, source := range event.Joined.Members {
-				userID := source.UserID
-				userInfo, err := b.GetUserProfile(userID)
-				if err != nil {
-					return err
-				}
-				msg := fmt.Sprintf("%s : %s", userInfo.DisplayName, userID)
-				userInfoMsgs = append(userInfoMsgs, msg)
-			}
-			if len(userInfoMsgs) > 0 {
-				userInfoMsg := strings.Join(userInfoMsgs, "\n")
-				pushMessages = append(pushMessages, linebot.GetTextMessage(userInfoMsg))
-			} else {
-				bs, err := json.Marshal(event)
-				if err != nil {
-					return err
-				}
-				msg := fmt.Sprintf("event:%s", string(bs))
-				pushMessages = append(pushMessages, linebot.GetTextMessage(msg))
-			}
-
-			pushReqs := &lineBotReqs.PushMessage{
-				To:       b.lineAdminID,
-				Messages: pushMessages,
-			}
-			if _, err := b.PushMessage(pushReqs); err != nil {
-				return err
-			}
-
 			replyMessges := []interface{}{
 				linebot.GetTextMessage("歡迎加入，跟我加入好友可以獲取更多社團的資訊喔!"),
 			}
@@ -56,6 +20,31 @@ func (b *ClubLineBot) handleMemberJoinedEvent(event *lineBotModel.MemberJoinEven
 				Messages:   replyMessges,
 			}
 			if _, err := b.ReplyMessage(replyReqs); err != nil {
+				return err
+			}
+
+			groupID := event.Source.GroupID
+			adminMessages := []string{
+				"member join group : " + groupID,
+			}
+			userInfoMsgs := []string{}
+			for _, source := range event.Joined.Members {
+				userID := source.UserID
+				userInfo, err := b.GetUserProfile(userID)
+				if err != nil {
+					adminMessages = append(adminMessages, userID)
+					adminMessages = append(adminMessages, err.Error())
+					continue
+				}
+				msg := fmt.Sprintf("%s : %s", userInfo.DisplayName, userID)
+				userInfoMsgs = append(userInfoMsgs, msg)
+			}
+			if len(userInfoMsgs) > 0 {
+				userInfoMsg := strings.Join(userInfoMsgs, "\n")
+				adminMessages = append(adminMessages, userInfoMsg)
+			}
+
+			if err := b.pushMessageToAdmin(adminMessages...); err != nil {
 				return err
 			}
 
