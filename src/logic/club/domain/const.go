69fd769450c62c8786db71791d9ae7e95842b19d
david511382 <david511382@yahoo.com.tw>
2022-03-29 09:02:06 +0800

    [修改] 羽球基本價格為32

--- src/logic/club/domain/const.go
+++ src/logic/club/domain/const.go
@@ -5,7 +5,7 @@ const (
 	ATTR_DATE_TIME_CMD = "date_time_cmd"
 	ATTR_CMD_BASE_PATH = "cmd_base_path"
 
-	PRICE_PER_BALL                 = 31
+	PRICE_PER_BALL                 = 32
 	ESTIMATE_BALL_CONSUME_PER_HOUR = 4
 	MONEY_UNIT                     = 5
 	PEOPLE_PER_HOUR                = 4
