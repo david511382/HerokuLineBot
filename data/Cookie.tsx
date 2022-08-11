c6b269508efbce0a5633cac8829fb63edb39729e
david511382 <david511382@yahoo.com.tw>
2022-08-11 11:01:26 +0800

    [修正] 取最後一個 cookie 錯誤

--- data/Cookie.tsx
+++ data/Cookie.tsx
@@ -22,9 +22,12 @@ export function GetCookies(): Map<any, any> {
 export function GetCookieOnKey(key: string): string {
     const RAW_COOKIES_STR = document.cookie
     const KEY_START_INDEX = RAW_COOKIES_STR.indexOf(key)
-    const RAW_KEY_COOKIES_STR = RAW_COOKIES_STR.substr(KEY_START_INDEX)
+    const RAW_KEY_COOKIES_STR = RAW_COOKIES_STR.substring(KEY_START_INDEX)
     const EQUAL_SYMBOL_INDEX = RAW_KEY_COOKIES_STR.indexOf('=')
-    const RAW_EQUAL_SYMBOL_COOKIES_STR = RAW_KEY_COOKIES_STR.substr(EQUAL_SYMBOL_INDEX)
-    const VALUE_END_INDEX = RAW_EQUAL_SYMBOL_COOKIES_STR.indexOf(';')
-    return RAW_EQUAL_SYMBOL_COOKIES_STR.substr(0, VALUE_END_INDEX - 1)
+    const VALUE_COOKIES_STR = RAW_KEY_COOKIES_STR.substring(EQUAL_SYMBOL_INDEX + 1)
+    const VALUE_END_INDEX = VALUE_COOKIES_STR.indexOf(';')
+    if (VALUE_END_INDEX === -1) {
+        return VALUE_COOKIES_STR.substring(0)
+    }
+    return VALUE_COOKIES_STR.substring(0, VALUE_END_INDEX - 1)
 }
